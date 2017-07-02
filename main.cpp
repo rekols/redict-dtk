a78958db3e4b3a5bad9939d147507067675d2ab5
rekols <rekols@foxmail.com>
2017-07-02 12:53:58 +0800

    add dtk set

--- main.cpp
+++ main.cpp
@@ -10,8 +10,17 @@ int main(int argc, char *argv[])
 
     DApplication a(argc, argv);
 
+    const char *descriptionText = QT_TRANSLATE_NOOP("MainWindow", "Rekols词典是一款简单易用的词典");
+    const QString acknowledgementLink = "https://github.com/rekols";
+
     if (a.setSingleInstance("rekols-dict"))
     {
+        a.setApplicationVersion("0.1");
+
+        a.setProductIcon(QPixmap::fromImage(QImage(":/image/logo.svg")));
+        a.setApplicationDescription(DApplication::translate("MainWindow", descriptionText) + "\n");
+        a.setApplicationAcknowledgementPage(acknowledgementLink);
+
         a.setTheme("dark");
         a.setWindowIcon(QIcon(":/image/logo.svg"));
 
